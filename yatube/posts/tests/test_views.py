79c234f01996b83e9afabd56938a4a4707dc2463
Gadzhi Shakhbanov <mister.tool@mail.ru>
2023-02-12 15:06:26 +0300

    3424324

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -182,7 +182,6 @@ class TaskPagesTests(TestCase):
                 self.assertEqual(
                     len(self.authorized.get(url).context['page_obj']), num,)
 
-
     def test_follow_authorized_author(self):
         """Проверка, что авторизованный пользователь может подписаться."""
         self.assertFalse(
